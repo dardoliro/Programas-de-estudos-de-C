b83861ca52085b0c87597692bdf2f8c5516f5128
dardoliro <77806012+dardoliro@users.noreply.github.com>
2021-07-21 14:47:51 -0300

    Add files via upload

---
+++ fatorial.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    // Variáveis
+    int x;
+    long long int fatorial = 1;
+    
+    printf("CALCULADORA DE FATORIAL\n\nDigite o valor de X: "); /* Inicia o programa */
+    scanf("%i", &x); /* Armazena o valor inserido pelo usuário no endereço de memória de X */
+
+    /* Laço FOR que armazena 
+    o produto de FATORIAL * X
+    na variável FATORIAL,
+    diminuindo X em 1,
+    a cada repetição */
+
+    for (x; x >=1; x--)
+    {
+        fatorial = fatorial * x;
+    }
+
+    /* Imprime o valor do fatorial na tela */
+    printf("%lli\n",fatorial);
+}
